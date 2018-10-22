746599e6e4346eb0a27b97864a3e0f6f8a2844aa
hubert1102 <44027774+hubert1102@users.noreply.github.com>
2018-10-22 22:54:33 +0200

    Create Arytmetyka.mli

---
+++ Arytmetyka.mli
@@ -0,0 +1,46 @@
+(* KONSTRUKTORY *)
+
+type wartosc = float * float;;
+
+let wartosc_dokladna x p = (( x -. p *. x /. 100., x +. p *. x /. 100.):wartosc);;
+let wartosc_od_do x y = ((x, y):wartosc);;
+let wartosc_dokladna x = ((x, x):wartosc);;
+
+(* SELEKTORY *)
+
+let in_wartosc (x:wartosc) (y:float) = 
+        if fst x > snd x
+                then (y >= fst x || y <= snd x)
+                else (y >= fst x && y <= snd x);;
+let min_wartosc (x:wartosc) = 
+        if fst x > snd x
+                then neg_infinity
+                else fst x
+;;
+let max_wartosc (x:wartosc) = 
+        if fst x > snd x
+                then infinity
+                else snd x
+;;
+let sr_wartosc (x:wartosc) = 
+        if fst x > snd x
+                then nan
+                else (min_wartosc x +. max_wartosc x) /. 2.
+;;
+
+(* MODYFIKATORY *)
+
+let plus (a:wartosc) (b:wartosc) =
+        if fst a > snd a then
+                if fst b > snd b then ((neg_infinity, infinity):wartosc) (* suma dwoch zbiorow z "luka" sumuja sie do zbioru od infinity do neg_infinity *)
+                else if (fst a -. snd a <= snd b -. fst b) then ((neg_infinity, infinity):wartosc) (* jak luka w a jest co najwyjzej wielkosci prz b to uda nam sie ja "zalatac" *)
+                        else ((fst a +. fst b, snd a +. snd b):wartosc)
+        else if fst b > snd b then
+                if (fst b -. snd b <= snd a -. fst a) then ((neg_infinity, infinity):wartosc)
+                        else  ((fst a +. fst b, snd a +. snd b):wartosc)
+        else  ((fst a +. fst b, snd a +. snd b):wartosc)
+;;
+
+let minus (a:wartosc) (b:wartosc) = plus a (-. snd b, -. fst b);;
+
+
